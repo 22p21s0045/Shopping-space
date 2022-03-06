f35a19a65c51ddc6850d2d8700285bab7ed64a78
lommel <monkeytitle@hotmail.com>
2022-03-06 13:25:44 +0700

    [ADD]-Svg-success

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -1,27 +1,40 @@
 import React from "react";
 import Image from "next/image";
 import Space from "../styles/img/Space shop Real.svg";
-import Alien from "../styles/img/Aliensmate.svg";
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-function Welcome() {
+import Alien from "../styles/img/AlienFix.svg";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+function Welcome(props: any) {
   return (
     <div>
       <Grid container alignItems="center" justifyContent="center">
-      <Box className="Welcome">
-      <Image src={Space} width={1130} height={675} className="Move"/>
-      </Box>
-      <Box>
-        <Typography variant="h1" style={{ fontFamily: "aquirebold", color: "white",fontSize: 96 }}>
-          WELCOME TO SPACE
-        </Typography>
-        <Image src ={Alien} width={300} height={300} />
+        <Box className="Welcome">
+          <Image src={Space} width={1130} height={675} className="Move" />
+        </Box>
+        <Box>
+          <Typography
+            variant="h1"
+            style={{ fontFamily: "aquirebold", color: "white", fontSize: 96 }}
+          >
+            WELCOME TO SPACE
+          </Typography>
+          <Typography
+            variant="h1"
+            style={{ fontFamily: "aquirebold", color: "white", fontSize: 24 }}
+            align="center"
+          >
+            HELLO {props.Name}
+          </Typography>
 
-      </Box>
+          <Image src={Alien} width={549} height={360} className="Move" />
+        </Box>
       </Grid>
     </div>
   );
 }
 
 export default Welcome;
+Welcome.defaultProps = {
+  Name: "GUEST",
+};
