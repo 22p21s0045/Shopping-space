fe6f8b4a88edc37d011600d27e3951df27030074
lommel <monkeytitle@hotmail.com>
2022-03-08 19:38:33 +0700

    [ADD]-map-notgood

--- components/Sponsor.tsx
+++ components/Sponsor.tsx
@@ -4,6 +4,7 @@ import Box from "@mui/material/Box";
 import Skeleton from "@mui/material/Skeleton";
 import Typography from "@mui/material/Typography";
 import { useQuery } from "react-query";
+import Image from "next/image";
 import { useEffect } from "react";
 import Alert from "@mui/material/Alert";
 import { Animated } from "react-animated-css";
@@ -11,12 +12,13 @@ import axios from "axios";
 
 function Sponsor() {
   const fetchSponsor = async () => {
-    const get = await axios.get("http://localhost:1337/api/sponsor/", {
+    const get = await axios.get("http://localhost:1337/api/sponsors/", {
       headers: {
         Authorization: `Bearer ${"54d7d3bc628a23e27006f902f3a4d249306ef1dbdd0757356c224b7da18bda92f3c58af9230ffe97478f1ec4f06afa4d58f43d0074743af05d28056e7fb15878334af0aa8af4af08233e77a1fab244188c04b3d4cfdd359b9d7e3b48a5348767fb73fcdc78fa0bf65e661988fb19b56cc509b5c6ac4eade569d48b0d335f8b9d"}`,
       },
     });
-    console.log(get);
+    return get
+   -
   };
   const { isLoading, isError, data, error } = useQuery("sponsor", fetchSponsor);
   if (isLoading) {
@@ -70,6 +72,14 @@ function Sponsor() {
           >
             SPONSOR
           </Typography>
+          {data.map((item: any) => {
+            return (
+              <Box>
+                <Image src={item.data.link} width={500} height={500} />
+              </Box>
+            );
+
+          })}
         </Box>
       </Grid>
     </div>
