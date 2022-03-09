098c885c952f4ceec64a4f913d02bfb922982ea2
lommel <monkeytitle@hotmail.com>
2022-03-09 20:05:01 +0700

    [ADD]-Explore-butoon

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -6,15 +6,19 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import { motion,useViewportScroll,useTransform} from "framer-motion";
+import { motion, useViewportScroll, useTransform } from "framer-motion";
+import {useState} from "react"
 function Welcome(props: any) {
   const { scrollYProgress } = useViewportScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [0.2, 2]);
+  const [SpeeedBg,setSpeeedBg] = useState(0)
+const HandleHover = () => {
+setSpeeedBg(5)
+}
   return (
     <div>
       <Grid container alignItems="center" justifyContent="center">
         <motion.div
-          animate={{ x: 100, y: 100}}
+          animate={{ x: 100 }}
           transition={{ type: "spring", stiffness: 100 }}
           initial={true}
         >
@@ -22,31 +26,43 @@ function Welcome(props: any) {
             <Image src={Space} width={1130} height={675} className="Move" />
           </Box>
         </motion.div>
-        <motion.div style ={{scaleY: scrollYProgress}}>
-        <Box className = "WelcomeT">
-          <Typography
-            variant="h1"
-            style={{ fontFamily: "aquirebold", color: "white", fontSize: 96 }}
-          >
-            WELCOME TO SPACE
-          </Typography>
-          <Typography
-            variant="h1"
-            style={{ fontFamily: "aquirebold", color: "white", fontSize: 24 }}
-            align="center"
-          >
-            HELLO {props.Name}
-          </Typography>
-        </Box>
+        <motion.div style={{ scaleY: scrollYProgress }}>
+          <Box className="WelcomeT">
+            <Typography
+              variant="h1"
+              style={{
+                fontFamily: "aquirebold",
+                color: "white",
+                fontSize: 96,
+                transform: "skewX(10deg)",
+              }}
+            >
+              WELCOME TO SPACE
+            </Typography>
+            <Typography
+              variant="h1"
+              style={{ fontFamily: "aquirebold", color: "white", fontSize: 24 }}
+              align="center"
+            >
+              HELLO {props.Name}
+            </Typography>
+          </Box>
         </motion.div>
         <Image src={Alien} width={549} height={360} className="Alien" />
+        <motion.div whileHover={{scale:1.5}} >
         <div style={{ paddingTop: 100 }}>
           <Button
-            style={{ fontFamily: "aquirebold", color: "white", fontSize: 48 }}
+            style={{
+              fontFamily: "aquirebold",
+              color: "white",
+              fontSize: 48,
+              transform: "skewX(10deg)",
+            }}
           >
             EXPLORE NOW
           </Button>
         </div>
+        </motion.div>
       </Grid>
     </div>
   );
