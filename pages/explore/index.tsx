c48e61b11fe2f9b4f2d2dc4e3af43d71663c541a
lommel <monkeytitle@hotmail.com>
2022-03-23 12:32:31 +0700

    [ADD]-getstaticprops-succcess

--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -5,24 +5,28 @@ import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import {GetStaticProps} from 'next'
 interface Props {
   name: string;
   path: string;
 }
-function index({menus}: Props) {
+function index(menus:Props) {
   //FIXME: height 100
+  console.log(menus)
   return (
     <div>
       <Navbar />
       
       <Grid container sx ={{height:"100%"}}   >
         <Grid item xs ={2} md={2} lg={1} >
-          <Box sx={{backgroundColor: '#000',height:'100%'}} alignItems='center' justifyItems ='center' textAlign='center'>
+          <Box sx={{backgroundColor: '#000',height:'100%',paddingTop:6}} alignItems='center' justifyItems ='center' textAlign='center'>
             <Avatar variant='rounded' sx={{margin:'auto'}}/>
             
-            <Button sx ={{fontFamily: "aquirebold"}}>
+            <Button sx ={{fontFamily: "aquirebold",width:'100%'}}>
               dff
             </Button>
+            
+            
 
           </Box>
         </Grid>
@@ -32,10 +36,10 @@ function index({menus}: Props) {
 }
 
 export default index
-export async function getstaticProps(){
+export const getStaticProps: GetStaticProps = async () => {
   const client = new ApolloClient({
     uri: "http://localhost:1337/graphql",
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache()
   });
   const { data } = await client.query({
     query: gql`
@@ -50,12 +54,11 @@ export async function getstaticProps(){
         }
       }
     }
-    
-    `,
+    `
   });
   return {
     props: {
-      menus: data.menus.data,
-    },
-  }
+      menus: data.menus
+    }
+  };
 }
