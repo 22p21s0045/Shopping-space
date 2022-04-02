1a612ac3cb2ea1cf0b2e98f0ab1dafc29d5111c1
lommel <monkeytitle@hotmail.com>
2022-04-03 00:54:43 +0700

    [ADD]-pre-data-slideshow

--- pages/explore/[id].tsx
+++ pages/explore/[id].tsx
@@ -5,16 +5,19 @@ import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/Store";
 import {useState,useEffect} from "react"
+interface State{
+  original: string;
+}
 function id() {
   const router = useRouter();
-  const [state, setState] = useState({});
+  const [state, setState] = useState<string[]>([]);
   const { id } = router.query;
   const product = useSelector((state: RootState) =>
     state.counter.products.find((p: { id: any }) => p.id == id)
   );
   
 useEffect(() => {
-  product.attributes.coverimage.data.map((item: { attributes: { url: string; }; }) =>setState({...state,original: item.attributes.url }));
+  product.attributes.coverimage.data.map((item: { attributes: { url: string; }; }) =>setState(oldstate=>([...oldstate,{original: item.attributes.url}])));
 },[])
   console.log(state)
   return (
