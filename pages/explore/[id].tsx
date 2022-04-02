4b4dc447fc5131d6cd292e78f7764f06e4db5c73
lommel <monkeytitle@hotmail.com>
2022-04-02 21:11:02 +0700

    [ADD]-coverimage

--- pages/explore/[id].tsx
+++ pages/explore/[id].tsx
@@ -4,17 +4,27 @@ import Grid from "@mui/material/Grid";
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/Store";
+import {useState,useEffect} from "react"
 function id() {
   const router = useRouter();
+  const [state, setState] = useState({});
   const { id } = router.query;
   const product = useSelector((state: RootState) =>
     state.counter.products.find((p: { id: any }) => p.id == id)
   );
+  
+useEffect(() => {
+  product.attributes.coverimage.data.map((item: { attributes: { url: string; }; }) =>setState({...state,original: item.attributes.url }));
+},[])
+  console.log(state)
   return (
     <div>
-      <Navbar />
+      <Navbar />  
 
       <Grid container>
+        <Grid item>
+
+        </Grid>
         <h1>This pag is ID:{id}</h1>
       </Grid>
     </div>
