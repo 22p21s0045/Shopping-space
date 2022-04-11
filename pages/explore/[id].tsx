3490edf30614fe06dbdd75c95434b7e74f94aee3
lommel <monkeytitle@hotmail.com>
2022-04-11 10:29:55 +0700

    [ADD]-count

--- pages/explore/[id].tsx
+++ pages/explore/[id].tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
 import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
+import Up from "../../styles/img/Button/Up.svg";
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/Store";
 import { styled } from "@mui/material/styles";
 import { useState, useEffect } from "react";
 import ImageGallery from "react-image-gallery";
-
+import Image from "next/image";
 interface State {
   original: string;
 }
@@ -18,19 +20,20 @@ const Root = styled("div")(({ theme }) => ({
   },
   [theme.breakpoints.up("md")]: {
     position: "relative",
-    
+
     paddingLeft: 100,
   },
-  
 }));
 function id() {
   const router = useRouter();
   const [state, setState] = useState([]);
+  
   const { id } = router.query;
   const product = useSelector((state: RootState) =>
     state.counter.products.find((p: { id: any }) => p.id == id)
   );
-
+  
+console.log(product);
   useEffect(() => {
     product.attributes.coverimage.data.map(
       (item: { attributes: { url: string } }) =>
@@ -46,25 +49,34 @@ function id() {
         )
     );
   }, []);
+  
   console.log(state);
   return (
     <div>
       <Navbar />
 
       <Grid container sx={{ marginTop: 5 }}>
-       <Root>
-        <Grid item lg={6} md={12} xs={12}>
-          <ImageGallery items={state} autoPlay={true} showPlayButton={false} />
-        </Grid>
-        </Root>
-        <Grid item lg={6} md={12} xs={12} sx ={{marginLeft:10}}>
-          <h1>{product.attributes.name}</h1>
-         
-          <h1 style ={{marginLeft:"40%"}}>{product.attributes.price} บาท</h1>
-          </Grid>
+        <Root>
           <Grid item lg={6} md={12} xs={12}>
-          <p>{product.attributes.description}</p>
+            <ImageGallery
+              items={state}
+              autoPlay={true}
+              showPlayButton={false}
+            />
           </Grid>
+        </Root>
+        <Grid item lg={6} md={12} xs={12} sx={{ marginLeft: 10 }}>
+          <h1>{product.attributes.name}</h1>
+
+          <h1 style={{ marginLeft: "40%" }}>{product.attributes.price} บาท</h1>
+          <IconButton>
+            <Image src={Up} />
+          </IconButton>
+          <h1>{product.quantity}</h1>
+        </Grid>
+        <Grid item lg={6} md={12} xs={12}>
+          <p>{product.attributes.description}</p>
+        </Grid>
       </Grid>
     </div>
   );
